c3d2efc76305987f1482613c9cf88f7ec840df1c
Michael Matheus <michaelmatheus2001@gmail.com>
2022-07-24 14:13:03 -0300

    feat: lesson #1 - what is typescript?

---
+++ Módulo 1 - Instalando e Entendendo/#1 - O que é TypeScript/index.ts
@@ -0,0 +1,59 @@
+/** 
+ * 
+ * O que é TypeScript?
+ * 
+ * O TypeScript é uma linguagem de código aberto desenvolvida pela Microsoft que foi construída em cima do JavaScript, que é muito difundido atualmente. Então esse "superset" foi criado para adicionar recursos de tipagem estáticas à linguagem original.
+ * 
+ * 
+ * Em outras palavras, temos todas as funcionalidades do JavaScript no TypeScript acrescidas de várias outras funcionalidades que caracterizam o TypeScript. (é um JavaScript bombado)
+ * 
+ * 
+ * Embora TypeScript seja um superset do JavaScript, na hora de compilar o código, todo TypeScript é convertido/transpilado para JavaScript.
+ * 
+ * 
+ * A pessoa desenvolvedora que estiver escrevendo códigos em TypeScript lidará com uma sintaxe simplificada, mais clara e suportada por vários Task Runners ou IDES, mas o seu código voltará a ser JS após transpilado.
+ * 
+ * 
+ * Isso se dá pelo browser não entender a sintaxe de outra linguagem de programação que não seja JavaScript.
+ * 
+ * 
+ * 
+ * 
+ * Qual a diferença entre TypeScript e JavaScript?
+ * 
+ * 
+ * Bom, já que TS nasceu de JS e veio para suprir algumas limitações, a seguir algumas diferenças básicas entre os dois:
+ * 
+ * 
+ * 
+ * ----------------------------------------------------------------
+ * Caracteristicas do TypeScript |  Caracteristicas do JavaScript |
+ * ---------------------------------------------------------------|
+ *       Tipagem estática        |        Tipagem dinâmica        |
+ *      Orientado a Objetos      |     Programação Estruturada    |
+ *           Genéricos           |             Funções            |
+ *          Namespaces           |           Prototypes           |
+ *          Decorators           |      Funções Construtoras      |
+ * ----------------------------------------------------------------
+ * 
+ *
+ * 
+ * Nesse sentido, dizer que TypeScript é outra linguagem de programação diferente do JavaScript não é verdade, pois tudo que o JavaScript oferece pode ser usado dentro de um código TypeScript e, na prática, a sintaxe JS é utilizada também.
+ * 
+ * 
+ * Porém, o contrario não é verdade, se optar por JavaScript, as funcionalidades inerentes ao TypeScript não poderão ser utilizadas, como a tipagem forte, por exemplo.
+ * 
+ * 
+ * Essa é uma das grandes vantagens da utilização do TypeScript:
+ * 
+ * Os tipos nos fornecem uma maneira de descrever um objeto não só mais precisamente, gerando uma documentação mais concisa, mas também uma validação do código em tempo de compilação.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Fonte: https://blog.geekhunter.com.br/introducao-a-typescript/#:~:text=O%20que%20%C3%A9%20TypeScript,-Representa%C3%A7%C3%A3o%20do%20superset&text=Typescript%20%C3%A9%20uma%20linguagem%20de,tipagem%20est%C3%A1ticas%20%C3%A0%20linguagem%20original.
+ */
