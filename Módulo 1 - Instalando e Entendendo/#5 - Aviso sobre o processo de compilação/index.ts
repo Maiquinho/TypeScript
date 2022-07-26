070e7b82ce568773880cb5311b9f87355c94c221
Michael Matheus <michaelmatheus2001@gmail.com>
2022-07-26 06:03:38 -0300

    feat: lesson #5 - advice about compiling process

---
+++ Módulo 1 - Instalando e Entendendo/#5 - Aviso sobre o processo de compilação/index.ts
@@ -0,0 +1,7 @@
+/**
+ * 
+ * Neste módulo realizamos o processo de compilação de uma forma um pouco mais didática apenas para vermos na prática o uso do TypeScript.
+ * 
+ * Nas próximas aulas vamos aprender a realizar outras configurações do TS para melhorar nosso processo de desenvolvimento nesse superset.
+ * 
+ */
