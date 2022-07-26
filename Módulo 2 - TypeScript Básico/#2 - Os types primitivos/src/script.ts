a9dc1c74dd05d32024383fa2c2df648efcacd55e
Michael Matheus <michaelmatheus2001@gmail.com>
2022-07-26 06:32:14 -0300

    feat: lesson #2 - the primitive types

---
+++ Módulo 2 - TypeScript Básico/#2 - Os types primitivos/src/script.ts
@@ -0,0 +1,5 @@
+let myName: string = 'Michael'; // só poderá ser definida como string
+
+let myAge: number = 21; // só poderá ser definida como number
+
+let programmer: boolean = true; // só poderá ser definida como boolean
