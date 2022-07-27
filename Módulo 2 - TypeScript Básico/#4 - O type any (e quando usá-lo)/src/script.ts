028ac34e7fea4c2c1e4e5d4dd76d9cb7c88cd841
Michael Matheus <michaelmatheus2001@gmail.com>
2022-07-27 08:47:45 -0300

    feat: lesson #4 - type any (when i use?)

---
+++ Módulo 2 - TypeScript Básico/#4 - O type any (e quando usá-lo)/src/script.ts
@@ -0,0 +1,10 @@
+/**
+ * 
+ * O type any permite que você salve valores de qualquer tipo em uma variável (não é recomendado seu uso em qualquer hora, pois isso "anula" o uso do typescript)
+ * 
+ * 
+ */
+
+const names:any = ['michael', 'matheus', 'paulo', 'joão', 'pedro'];
+
+names.push(556);
