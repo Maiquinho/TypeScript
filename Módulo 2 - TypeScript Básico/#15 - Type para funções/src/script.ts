a1db626dc9d565f347010fbf3fb79707142dfc54
Michael Matheus <michaelmatheus2001@gmail.com>
2022-08-01 14:26:43 -0300

    feat(exemples): #15 - function types

---
+++ Módulo 2 - TypeScript Básico/#15 - Type para funções/src/script.ts
@@ -0,0 +1,17 @@
+type MathFunction = (n1: number, n2: number) => number;
+
+const add: MathFunction = (n1, n2) => {
+    return n1 + n2
+}
+
+const subtract: MathFunction = (n1, n2) => { 
+    return n1 - n2
+}
+
+const multiply: MathFunction = (n1, n2) => {
+    return n1 * n2
+}
+
+const division: MathFunction = (n1, n2) => {
+    return n1 / n2
+}
